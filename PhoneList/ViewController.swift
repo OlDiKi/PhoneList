733e35b2da5925e2aa42efc8934d0012ce99b4d3
OlDiKi <delorian101allgood@gmail.com>
2021-12-29 19:01:43 +0300

    Initial Commit

---
+++ PhoneList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhoneList
+//
+//  Created by Дмитрий Олейнер on 29.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
