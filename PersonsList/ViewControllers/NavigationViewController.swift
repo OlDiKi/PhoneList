eacffdd36b1257ff48fd080392741ea7a1506cf5
OlDiKi <delorian101allgood@gmail.com>
2021-12-29 23:58:52 +0300

    Setup mainboards

---
+++ PersonsList/ViewControllers/NavigationViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NavigationViewController.swift
+//  PersonsList
+//
+//  Created by Дмитрий Олейнер on 29.12.2021.
+//
+
+import UIKit
+
+class NavigationViewController: UITabBarController {
+
+  
+}
