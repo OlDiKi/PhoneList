4157fa9dd4b12187c346780cbec78739c82d0605
OlDiKi <delorian101allgood@gmail.com>
2021-12-30 02:21:08 +0300

    basic logic for first exercise

--- PersonsList/ViewControllers/AboutPersonViewController.swift
+++ PersonsList/ViewControllers/AboutPersonViewController.swift
@@ -9,21 +9,16 @@ import UIKit
 
 class AboutPersonViewController: UIViewController {
 
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    var person: Person!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        navigationItem.title = person.fullName
+        emailLabel.text = "Email: \(person.email)"
+        phoneLabel.text = "Phone: \(person.phone)"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+       
--- PersonsList/ViewControllers/NavigationViewController.swift
+++ PersonsList/ViewControllers/NavigationViewController.swift
@@ -9,5 +9,26 @@ import UIKit
 
 class NavigationViewController: UITabBarController {
 
-  
+   private let persons = Person.getPerson()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        personsAdd()
+    }
+}
+
+extension NavigationViewController {
+    
+    private func personsAdd () {
+        guard let viewControllers = self.viewControllers else { return }
+        
+        for viewController in viewControllers {
+            guard let navigationVC = viewController as? UINavigationController else { return }
+            if let personsVC = navigationVC.topViewController as? PersonsTableViewController {
+                personsVC.persons = persons
+            } else if let personsInfoVC = navigationVC.topViewController as? PersonsInfoTableViewController {
+                personsInfoVC.persons = persons
+            }
+        }
+    }
 }
